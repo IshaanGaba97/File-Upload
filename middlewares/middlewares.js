a11734b8d04c8acd48854530853064febca120b6
Ishaan Gaba <ishaangaba97@gmail.com>
2024-01-30 23:08:25 +0530

    change

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -4,7 +4,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000 }, // 1MB limit
+  limits: { fileSize: 1000000 }, 
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
       cb(null, true);
